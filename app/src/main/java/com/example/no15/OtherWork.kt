1dcc2f5b3b6872f3d7506123b0da347d767d3ed7
Duck <40841131@gm.nfu.edu.tw>
2022-04-28 23:02:29 +0800

    新增其他工作頁面

---
+++ app/src/main/java/com/example/no15/OtherWork.kt
@@ -0,0 +1,11 @@
+package com.example.no15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherWork : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_work)
+    }
+}
