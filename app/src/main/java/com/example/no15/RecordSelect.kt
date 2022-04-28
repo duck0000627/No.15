b13ba8f766dc3837de30708cbb3e537b9e4d70c1
Duck <40841131@gm.nfu.edu.tw>
2022-04-28 22:18:06 +0800

    基本版面設計

---
+++ app/src/main/java/com/example/no15/RecordSelect.kt
@@ -0,0 +1,11 @@
+package com.example.no15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_select)
+    }
+}
