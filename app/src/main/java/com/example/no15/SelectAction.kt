df754eac813657fc787f9ebe6f5855e235b86f49
Duck <40841131@gm.nfu.edu.tw>
2022-04-28 22:54:01 +0800

    從動作頁面切換到工作選擇頁面

--- app/src/main/java/com/example/no15/SelectAction.kt
+++ app/src/main/java/com/example/no15/SelectAction.kt
@@ -1,11 +1,17 @@
 package com.example.no15
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_select_action.*
 
 class SelectAction : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_action)
+
+        btn_record.setOnClickListener{
+            startActivity(Intent(this, RecordSelect::class.java))
+        }
     }
 }
