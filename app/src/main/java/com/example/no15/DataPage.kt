12347ccd846170d1518303b2b0621b71d4744ea1
Duck <40841131@gm.nfu.edu.tw>
2022-05-04 22:52:30 +0800

    增加可開關的側邊欄

--- app/src/main/java/com/example/no15/DataPage.kt
+++ app/src/main/java/com/example/no15/DataPage.kt
@@ -12,7 +12,15 @@ class DataPage : AppCompatActivity() {
         setContentView(R.layout.activity_data_page)
 
         add_record.setOnClickListener{
-            startActivity(Intent(this, FarmWork::class.java))
+            startActivity(Intent(this, FarmWork::class.java))                //按按鈕切換頁面
+        }
+
+        toolbar.setNavigationOnClickListener{
+            if (layout_drawer.isDrawerOpen(navigation_drawer)){            //如果側邊欄是開的就關掉
+                layout_drawer.closeDrawer(navigation_drawer)
+            }else{
+                layout_drawer.openDrawer(navigation_drawer)                //如果側邊欄是關的就打開
+            }
         }
 
     }
