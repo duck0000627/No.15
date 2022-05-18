42c787209b3c86dcc822bb954f290cd25e772e1f
Duck <40841131@gm.nfu.edu.tw>
2022-05-18 22:16:22 +0800

    回上一頁的程式

--- app/src/main/java/com/example/no15/FarmWork.kt
+++ app/src/main/java/com/example/no15/FarmWork.kt
@@ -1,11 +1,19 @@
 package com.example.no15
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_farmwork.*
 
 class FarmWork : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_farmwork)
+
+        toolbar_Farmwork.inflateMenu(R.menu.add_toolbar)   //toolbar樣式載入
+
+        toolbar_Farmwork.setNavigationOnClickListener{
+            startActivity(Intent(this,DataPage::class.java))     //案箭頭回上一頁
+        }
     }
 }
