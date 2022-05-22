3754ec0337f1de0cdec426955972d6f51d087b1a
Duck <40841131@gm.nfu.edu.tw>
2022-05-22 18:36:03 +0800

    其他頁面

--- app/src/main/java/com/example/no15/OtherWork.kt
+++ app/src/main/java/com/example/no15/OtherWork.kt
@@ -1,11 +1,34 @@
 package com.example.no15
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_muck_work.*
+import kotlinx.android.synthetic.main.activity_other_work.*
 
 class OtherWork : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_work)
+
+        toolbar_Otherwork.inflateMenu(R.menu.add_toolbar)   //toolbar樣式載入
+
+        toolbar_Otherwork.setNavigationOnClickListener{
+            Log.d("Alert","1")
+            AlertDialog.Builder(this)    //會跳一個提示框
+                .setTitle("捨棄")
+                .setMessage("確定捨棄紀錄內容?")
+                .setNegativeButton("取消"){
+                        dialog, which->
+                    Toast.makeText(this,"取消", Toast.LENGTH_SHORT).show()
+                }
+                .setPositiveButton("捨棄") {dialog,which ->
+                    startActivity(Intent(this, FarmWork::class.java)) //案箭頭回上一頁
+                }.show()
+        }
+
     }
 }
